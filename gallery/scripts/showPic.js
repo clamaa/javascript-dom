03fe7516edb10a56d66a72b2bbdc8f7aeb9c018e
clamaa <clark.mazhiqiang@gmail.com>
2014-03-23 11:23:40 +0800

    separate js and event from basic html

---
+++ gallery/scripts/showPic.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * Created by mazhiqiang on 14-3-22.
+ */
+
+function showPic(whichpic) {
+    if(!document.getElementById("placeholder")) return false;
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    if(placeholder.nodeName != 'IMG') return false;
+    placeholder.setAttribute("src", source);
+    if(document.getElementById("description")){
+        var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
+        var description = document.getElementById("description");
+        if(description.firstChild.nodeType == 3){
+            description.firstChild.nodeValue = text;
+        }
+    }
+    return true;
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var links = document.getElementById("imagegallery").getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            return !showPic(this);
+        }
+        links[i].onkeypress = links[i].onclick;
+    }
+}
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != "function") {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(prepareGallery)
+
